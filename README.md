6861e0f34aef6ba8f2099f9af06fc134fbe7b43f
chandut027 <chandut027@gmail.com>
2018-02-25 15:44:54 +0100

    adding basic tests for dispatcher and domains

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # aem-gatling
 Performance tests for AEM projects
+
+Gatling is a Scala based open-source load test tool which utilises asynchronous concurrency and a DSL for scripting.
+
+### To run the tests
+
+    simply execute the following command for the home classes:
+
+    mvn -Denv=docker -DuserPerSec=2 -Dduration=10 gatling:execute -Dgatling.simulationClass=simulations.LoadTestAllResources
+
+    mvn -Denv=docker -DuserPerSec=2 -Dduration=10 gatling:execute -Dgatling.simulationClass=simulations.LoadTestOnlyHtmls
+
+### Available tests
+
+1. LoadTestOnlyHtmls 
+	a. will load test only given urls
+	b. This hits ur dispatcher url
+2. LoadTestAllResources
+	a. will load test current urls along with sub resource like css,js, images .. except child htmls..
+	b. This hits ur domain url
+
+### Available environments
+
+1. uat
+2. qa
+3. docker(local)
---
+++ src/test/scala/simulations/LoadAllResourceTest.scala
@@ -0,0 +1,66 @@
+package simulations
+
+import java.io.File
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.duration._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import com.typesafe.config._
+import io.gatling.http.protocol.HttpProtocolBuilder.toHttpProtocol
+import io.gatling.http.request.builder.HttpRequestBuilder.toActionBuilder
+import io.gatling.http.action.sync.HttpRequestActionBuilder
+import io.gatling.core.structure.ChainBuilder
+import scala.Vector
+
+class LoadAllResourceTest extends Simulation {
+
+  /* get maven args. and assign to vars */
+  val env = System.getProperty("env")
+
+  val userPerSec:Int=System.getProperty("userPerSec").toInt
+  val duration:Int=System.getProperty("duration").toInt
+
+  /* Pretty print to console using interpolation*/
+  println(s"userPerSec :  $userPerSec")
+  println(s"duration :  $duration")
+  println(s"environment :  $env")
+
+  
+  /* based on argument given, e.g. if QA is passed to maven then load qa.conf. Then assign to conf variable */
+  val parsedConfig = ConfigFactory.parseFile(new File("src/test/resources/" + env + ".conf"))
+  val conf = ConfigFactory.load(parsedConfig)
+  
+  var baseUrl = conf.getString("load.domain") + conf.getString("load.basePath") 
+
+  println(s"domain :  $baseUrl")
+		  
+  val locales = jsonFile("locales.json")
+  val pages = jsonFile("pages.json")
+  var count = 0;
+  var httpLists = new ListBuffer[ChainBuilder]()
+  for (localeNum <- 0 to locales.records.size -1) { 
+	  for (pageNum <- 0 to pages.records.size -1) { 
+		  count +=1
+		  val path = ""+locales.records(localeNum)("locale") + pages.records(pageNum)("page")+ "?skipCache=true"
+		  println(s"url$count :  $path")
+		  httpLists += exec(http("Page: " + pages.records(pageNum)("page")).get(path))
+	  }
+	}
+  val users = (userPerSec * duration) / (locales.records.size * pages.records.size)
+  println(s"users :  $users")
+  val scnItems = scenario("My Scenari").exec(httpLists.toList)
+  
+
+
+   val httpProtocol = http
+    .baseURL(baseUrl).inferHtmlResources(BlackList(""".*\.html""")).disableCaching
+    
+  val execution = scnItems
+    .inject(rampUsers(users) over duration)
+  setUp(execution).protocols(httpProtocol).assertions(
+    global.responseTime.max.lt(5000),
+    global.responseTime.mean.lt(800),
+    global.successfulRequests.percent.gt(95))
+
+}
